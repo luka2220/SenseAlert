96771f4462ba8dfa6100a38451ea44d25da3ce22
App-Learn <71737181+App-Learn@users.noreply.github.com>
2021-04-23 11:45:03 -0400

    Update Hardware code

--- Firmware/Motion.py
+++ Firmware/Motion.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import sys
 import os
+#from time import sleep
+from gpiozero import CPUTemperature
 from pyrebase import pyrebase
+from pusher_push_notifications import PushNotifications    
 import time
 
 
-#SenseAlert database config                
+
+
 config = {
   "apiKey": "APIKEY",
   "authDomain": "yourProjectID.fdirebaseapp.com",
@@ -15,13 +19,18 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-auth = firebase.auth()   
+auth = firebase.auth()    
 
-email = "your_email"           
-password ="your_password"                         
+email = "youremail"          
+password ="yourpassword"                        
 user = auth.sign_in_with_email_and_password(email, password)    
 
 db = firebase.database()
+ 
+beams_client = PushNotifications(                                                      
+    instance_id='your-id',                            
+    secret_key='yourkey',     
+)                                                                                       
 
 
 try:
@@ -48,9 +57,10 @@ def turnon():
     print ('LED is ON')
     return
 
-def measure_temp():                                                
-        temp = os.popen("vcgencmd measure_temp").readline()        
-        return (temp.replace("temp=",""))                       
+def measure_temp():                                              
+    cpu = CPUTemperature()
+    return cpu.temperature
+
     
 def main():  
     global PIN_13
@@ -60,37 +70,78 @@ def main():
     #global total_count
     
     data = {"Motion": counter}
+    #db.child("message").push(data)
     data_temprature ={"Temperature": measure_temp()}             
     
     print ('Start LED blinking')    
     GPIO.setmode(GPIO.BCM)    
     GPIO.setwarnings(False)
     
-    GPIO.setup(GPIO_Out_List, GPIO.OUT)  #Initialize the led pin as output
-    GPIO.setup(GPIO_In_List, GPIO.IN)    #Initializing sensor pin as input
+    GPIO.setup(GPIO_Out_List, GPIO.OUT)  
+    GPIO.setup(GPIO_In_List, GPIO.IN)    
+   
+    data_temperature ={"Temperature": measure_temp()}        
+    db.child("/Temperature_Sensor").update(data_temperature,user['idToken'])  
+    check_Temp = db.child("/Temperature_Sensor").get(user['idToken']).val()
 
 
     while True:
         data_temperature ={"Temperature": measure_temp()}         
         db.child("/Temperature_Sensor").update(data_temperature,user['idToken'])  
-        check_UI = db.child("Control_Motion_Sensor").get(user['idToken']).val()        
+        check_Temp = db.child("/Temperature_Sensor").get(user['idToken']).val()
+        check_UI = db.child("Control_Motion_Sensor").get(user['idToken']).val()               
+        data_temperature ={"Temperature": measure_temp()}        
+        db.child("/Temperature_Sensor").update(data_temperature,user['idToken'])  
+        check_Temp = db.child("/Temperature_Sensor").get(user['idToken']).val()
+        if (check_Temp["Temperature"] > 28) :
+            response = beams_client.publish_to_interests(
+            interests=['hello'],
+            publish_body={
+                'apns': {
+                    'aps': {
+                        'alert': 'High Temperature!'
+                    }
+                },
+                'fcm': {
+                    'notification': {
+                        'title': 'High Temperature!',
+                        'body': 'Please check and make sure everyone is safe'
+                    }
+                }
+            }
+        )
         if check_UI["Status"] == 'True':
             i=GPIO.input(PIN_7)
 
-            if i==0:                 #When output from motion sensor is LOW
+            if i==0:                
                 print ("No Motion",i)
                 turnoff()
                 time.sleep(1)
-            elif i==1:               #When output from motion sensor is HIGH
+            elif i==1:               
                 print ("Motion",i)
                 turnon()
                 counter+=1
-                data = {"Motion": counter}   
-                db.child("/Motion_Counter").update(data, user['idToken'])  
-                                                
-                                               
-                                               
-                time.sleep(5)                
+                data = {"Motion": counter}  
+                db.child("/Motion_Counter").update(data, user['idToken']) 
+                response = beams_client.publish_to_interests(                      
+                    interests=['hello'],                                           
+                    publish_body={                                                  
+                        'apns': {                                                   
+                            'aps': {                                                
+                                'alert': 'Motion Detected!'                                   
+                            }                                                       
+                        },                                                         
+                        'fcm': {                                                     
+                            'notification': {                                       
+                                'title': 'Motion Detected!',                                    
+                                'body': 'Please check and make sure everyone is safe'                           
+                            }                                                      
+                        }                                                               
+                    }                                                              
+                )                                                                 
+                                         
+
+                time.sleep(5)               
             i=0
         else:
             print(check_UI["Status"]);
@@ -99,6 +150,7 @@ def main():
     
 if __name__ == "__main__":
   main()
+
 
 
 
